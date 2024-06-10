84158c583cf1a0405b39662796f330d8f13c7aac
Smarchoks <zubarskiy@list.ru>
2024-06-10 12:57:56 +0300

    bam

--- dadada/src/slader/slider.js
+++ dadada/src/slader/slider.js
@@ -5,6 +5,7 @@ import slide_1 from "../../src/Img/slide_1.JPG";
 import slide_2 from "../../src/Img/slide_2.jpg";
 import slide_3 from "../../src/Img/slider_3.jpg";
 import slide_4 from "../../src/Img/slider_4.jpg";
+import slide_5 from "../../src/Img/slider_4.jpg";
 
 
 const flickityOptions = {
@@ -13,7 +14,7 @@ const flickityOptions = {
 
 const coffeeSlider = {
   id: [1, 2, 3, 4, 5],
-  title: ["Эспрессо", "Капучино", "Латте", "Американо", "Раф"],
+  title: ["Наши фирменные стрипсы", "Бургеры с курицей", "Сладкая курочка", "Множество закусок", ],
   images: [slide_1, slide_2, slide_3, slide_4],
 };
 
@@ -38,9 +39,9 @@ export default function ScrollElem() {
             >
               <div className="textContainer">
                 <h2 className="scrollName">{coffeeSlider.title[index - 1]}</h2>
-                <h1 className="titleScrlolElemMenu">Кофейный Магазин</h1>
+                <h1 className="titleScrlolElemMenu">В сети ресторанов LOS POLLOS</h1>
                 <h3 className="title2ScrolElemMenu">
-                  Наслаждайтесь Своим Кофе Весь День
+                  Попробуйте все наши позиции!
                 </h3>
                 <a href="#" className="btnShop">
                   Купить
