3b6192bae47b468421a9fcb22e6e824b1726fe91
Smarchoks <zubarskiy@list.ru>
2024-06-17 03:34:17 +0300

    yra

--- dadada/src/History/history.js
+++ dadada/src/History/history.js
@@ -1,11 +1,31 @@
 import "./history.css";
 
+import gus from "../Img/fff.jpg";
+import kisti from "../Img/cms-bg-pattern.png";
+
 export default function Header() {
-    return (
+  return (
+    <div className="lala">
       <div className="container_history">
-        <p>
-            asdasdasdassssAAAAAAAAAAAAA
-        </p>
+        <div className="block_history">
+          <ul className="block_title">
+            <li className="title_history_1">История</li>
+            <li className="title_history_2">Нашего ресторана</li>
+          </ul>
+          <p className="text_history">
+            LOS POLLOS - это не просто закусочная, это легенда вкуса и качества,
+            которая начала свой путь еще в далекие времена. Здесь каждый кусочек
+            курицы пропитан заботой и традициями, чтобы порадовать ваши вкусовые
+            рецепторы и оставить незабываемый вкус в памяти на всю жизнь.
+          </p>
         </div>
+        <div className="photo_gus">
+          <img src={gus} width={320} />
+        </div>
+      </div>
+      <div className="kisti">
+        <img src={kisti} width={1800} />
+      </div>
+    </div>
   );
 }
